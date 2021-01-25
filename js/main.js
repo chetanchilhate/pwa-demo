a33e29edf246538e1141b297e90305b57f31498c
Chetan Chilhate <cchilhate@medline.com>
2021-01-25 20:22:18 +0530

    added service worker

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Success .... 🙂");
+console.log("PWA is Running! .... 😀");
+
+window.addEventListener("load", () => {
+    if ("serviceWorker" in navigator) {
+        registerServiceWorker().catch(err => console.log("Error: ", err));
+    }
+  });
+
+async function registerServiceWorker() {
+    try {
+        const reg = await navigator.serviceWorker.register("/sw.js");
+        console.log('Registered! 😎 ', reg);
+    } catch (err) {
+        console.log('😭 Registration failed: ', err);
+    }
+}
